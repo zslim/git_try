e2494a96857efe4f0dec8ed5a5fe62c7109d574c
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-06-19 13:00:32 +0200

    no idea what the change was

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
-print("first version of git trial code")
+print("first version of git trial code - and I changed this")
 
 print("this is something new in the code")
 
-h = 3*6
+h = 3 * 6
 print(h)
+
+print("ok so yet another add to the code")
